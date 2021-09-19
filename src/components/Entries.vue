8f4e54f7b1094f8414fb2ab5f3b58ceae04bc691
N1K1TAS95 <46042802+N1K1TAS95@users.noreply.github.com>
2021-09-19 17:09:12 +0200

    Improved sorting entries

--- src/components/Entries.vue
+++ src/components/Entries.vue
@@ -4,18 +4,18 @@
              v-for="(doc, index) in documents" :key="doc.id"
         >
             <div class="row">
-                <div class="col-2">
+                <div class="col-lg-2">
                     <div class="badge bg-primary">
                     {{ new Date(doc.date).toLocaleDateString() }}
                     </div>
                 </div>
-                <div class="col-3">
+                <div class="col-lg-3 mt-2 mt-lg-0">
                     {{ doc.category }}
                 </div>
-                <div class="col">
+                <div class="col-lg mt-2 mt-lg-0">
                     {{ doc.description }}
                 </div>
-                <div class="col-2 text-end">
+                <div class="col-lg-2 mt-2 mt-lg-0 text-end">
                     <div :class="['badge', {'bg-success' : doc.type === 'ENTRY'}, {'bg-danger' : doc.type === 'EXIT'}]">
                         <span v-if="doc.type === 'ENTRY'">+</span>
                         <span v-else>-</span>
