aa788320cb79d746efbec87fa76d5d94c88093f2
N1K1TAS95 <46042802+N1K1TAS95@users.noreply.github.com>
2021-09-26 20:01:25 +0200

    Improved navbar style

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,17 +5,20 @@
                 <i class="bi bi-graph-up me-2"></i>
                 {{ t('app_name') }}
             </router-link>
-            <div class="justify-content-end">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="justify-content-end collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav my-auto">
                     <li class="nav-item">
                     </li>
                     <li class="nav-item nav-link active" v-if="user">
                         {{ user.displayName }}
                     </li>
-                    <li class="nav-item my-auto">
+                    <li class="nav-item my-lg-auto mt-2 me-2">
                         <button class="btn btn-sm btn-outline-light" @click="handleClick">{{ t('logout') }}</button>
                     </li>
-                    <li class="nav-item mx-2 my-auto">
+                    <li class="nav-item my-lg-auto mt-2 me-auto">
                         <LanguageSelect/>
                     </li>
                 </ul>
